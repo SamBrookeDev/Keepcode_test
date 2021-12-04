c3356c15d0bf64674f7db6cc4b8ddb170aa62738
Sam Brookeemail <kleychin@gmail.com>
2021-12-04 21:44:56 +0300

    add full HashMap

--- src/main/java/Task_1/Main.java
+++ src/main/java/Task_1/Main.java
@@ -3,45 +3,49 @@ package Task_1;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Map<String, ArrayList<String>> numbersMap = null;
+        Map<String, ArrayList<String>> numbersMap = new HashMap<String, ArrayList<String>>();
 
-        List<Integer> codesList = new ArrayList<Integer>();
+
         JsonObject obj = getRequest("https://onlinesim.ru/api/getFreeCountryList");
         JsonArray codesArray = obj.getAsJsonArray("countries");
 
         for (int i = 0; i < codesArray.size(); i++) {
             if (!codesArray.get(i).getAsJsonObject().get("country").isJsonNull()) {
                 int code = codesArray.get(i).getAsJsonObject().get("country").getAsInt();
-                codesList.add(code);
-            }
-        }
+                String country = codesArray.get(i).getAsJsonObject().get("country_text").getAsString();
 
-        for (Integer code : codesList
-        ) {
-            List<String> numberList = new ArrayList<String>();
-            JsonObject object = getRequest("https://onlinesim.ru/api/getFreePhoneList?country=" + code);
-            JsonArray numArray = object.getAsJsonArray("numbers");
-            for (int i = 0; i < numArray.size(); i++) {
-                if (!numArray.get(i).getAsJsonObject().get("full_number").isJsonNull()) {
-                    String number = numArray.get(i).getAsJsonObject().get("full_number").getAsString();
-                    numberList.add(number);
+                JsonObject object = getRequest("https://onlinesim.ru/api/getFreePhoneList?country=" + code);
+                JsonArray numArray = object.getAsJsonArray("numbers");
+                ArrayList<String> numberList = new ArrayList<String>();
+
+                for (int j = 0; j < numArray.size(); j++) {
+                    if (!numArray.get(j).getAsJsonObject().get("full_number").isJsonNull()) {
+                        String number = numArray.get(j).getAsJsonObject().get("full_number").getAsString();
+                        numberList.add(number);
+                    }
                 }
+                numbersMap.put(country, numberList);
             }
-
         }
+        //numbersMap.entrySet().forEach(System.out::println);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(numbersMap);
+
+
+
 
     }
 
